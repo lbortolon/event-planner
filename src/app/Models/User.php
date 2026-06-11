5d80853f4e2768f530bbfc65174ff9a7ee5182fe
Luca Bortolon <luca.bortolon.95@gmail.com>
2026-06-11 20:01:15 +0200

    Add AuthController with register, login, logout, me endpoints + findByEmail on User model

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -7,11 +7,12 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -33,6 +34,11 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    public static function findByEmail(string $email): ?self
+    {
+        return static::where('email', $email)->first();
+    }
 
     /**
      * Get the attributes that should be cast.
